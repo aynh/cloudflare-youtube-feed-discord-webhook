950faf87bcb3c26b31835e1b12138c11606a67b5
Aynh <99479536+Aynh@users.noreply.github.com>
2022-09-12 20:07:02 +0800

    chore(deps): upgrade ts-jest

--- jest.config.cjs
+++ jest.config.cjs
@@ -3,10 +3,13 @@ module.exports = {
   preset: 'ts-jest/presets/default-esm',
   testEnvironment: 'node',
   extensionsToTreatAsEsm: ['.ts'],
-  globals: {
-    'ts-jest': {
-      useESM: true,
-    },
+  transform: {
+    '.ts': [
+      'ts-jest',
+      {
+        useESM: true,
+      },
+    ],
   },
   reporters: ['default', 'github-actions'],
 }
